0c0d0eb0ebd7125075d2533a6b1bd56793851662
Visgean Skeloru <visgean@gmail.com>
2014-02-02 23:21:26 +0100

    redactor snippet translation

--- text_admin/translation.py
+++ text_admin/translation.py
@@ -1,9 +1,10 @@
 from modeltranslation.translator import translator, TranslationOptions
 
-from .models import Snippet
+from .models import Snippet, RedactorSnippet
 
 class AddendumTranslationOptions(TranslationOptions):
     fields = ('text',)
 
 translator.register(Snippet, AddendumTranslationOptions)
+translator.register(RedactorSnippet, AddendumTranslationOptions)
 
