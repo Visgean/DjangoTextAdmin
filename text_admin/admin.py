45c862fac3dc3cb45c51f11accbe28fb89e105e6
Visgean Skeloru <visgean@gmail.com>
2014-02-02 23:24:10 +0100

    debugging translation registration

--- text_admin/admin.py
+++ text_admin/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
+from modeltranslation.translator import translator, TranslationOptions
 
 from .models import Snippet, RedactorSnippet
+
+class AddendumTranslationOptions(TranslationOptions):
+    fields = ('text',)
+
+translator.register(Snippet, AddendumTranslationOptions)
+translator.register(RedactorSnippet, AddendumTranslationOptions)
+
 
 
 class SnippetAdmin(TranslationAdmin):
